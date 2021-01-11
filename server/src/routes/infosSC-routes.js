43a74bd90ebe3f4ea710e990f8cadeba4da15b8d
Quentin Gibon <quentingibon@wanadoo.fr>
2021-01-11 18:34:33 +0100

    L'accès au storage du SC fonctionne

--- server/src/routes/infosSC-routes.js
+++ server/src/routes/infosSC-routes.js
@@ -1,39 +1,25 @@
 const express = require('express')
+const fetch = require('node-fetch');
 
 const router = new express.Router()
 
 const apiBaseUrl = 'https://api.better-call.dev/v1'
 
-// Fonction réutilisable pour faire des appels à l'API BCD
-function fetchBCD(path) {
-
-    return fetch(`${apiBaseUrl}${path}`, {
-        method: "GET",
-        headers: {
-            Accept: 'application/json'
-        }
-    })
-    .then(fetchResponse => {
-        return fetchResponse.json();
-    })
-    .then(responseData => {
-        const errorCode = responseData.status.error_code
-        if (errorCode != 0) {
-            throw new Error(responseData.status.error_message)
-        }
-        return responseData.data
-    })
-}
-
 router.get('/', function getRoot(req, res) {
 
     const path = '/contract/delphinet/KT1Wja5vWzUP15fWpAYFdGYHZmBZbtuHzG4H/storage'
 
-    fetchBCD(path)
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+      
+    fetch(`${apiBaseUrl}${path}`, requestOptions)
+    .then(response => response.json())
     .then(storage => {
         res.json({
             success: true,
-            storage
+            storage // organiser les données selon le json
         })
     })
     .catch(error => {
