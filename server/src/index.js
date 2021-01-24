0b500e11606be352a716885c1095c557c7a5000c
Quentin Gibon <quentingibon@wanadoo.fr>
2021-01-24 18:24:33 +0100

    BDD Henry après la réunion

--- server/src/index.js
+++ server/src/index.js
@@ -14,21 +14,12 @@ app.use('/api/v1', router)
 app.use(bodyParser.json())
 
 // BDD region
-const mongoose = require("mongoose");
-const morgan = require("morgan");
 const cors = require("cors");
-mongoose.connect('mongodb+srv://liviator:liviator@cluster0.h11ry.mongodb.net/Cluster0?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(morgan("dev"));
 
-const userRoutes = require("./user/route/user"); 
-app.use("/user", userRoutes);
+const clientRoutes = require('./user/route/client.js')
+app.use("/client", clientRoutes);
 // end region BDD
 
 app.listen(port, () => {
---
+++ server/src/user/controller/client.js
@@ -0,0 +1,45 @@
+const mysql = require('mysql')
+const pool = require('../model/dbpool')
+const bcrypt = require('bcrypt')
+
+
+exports.register = (req, res, next) => {
+    bcrypt.hash(req.body.pwd, 10)
+        .then(hash => {
+            pwd = hash
+            sql='INSERT INTO user (pwd, pub_key, rôle, nom, email) VALUES (?,?,?,?,?)'
+            var insert = [pwd,req.body.pub_key,req.body.rôle,req.body.nom,req.body.email]
+            sql = mysql.format(sql,insert);
+            pool.query(sql,function(error,results,fields){
+            if(error) throw error;
+            })              
+            res.status(201).json({ message:"Inscription succès" })
+        })
+        .catch(error => res.status(400).json({ error: "erreur" }));
+    
+};
+
+exports.login = (req, res, next) => {
+
+    
+    sql= 'SELECT * FROM user WHERE email = ?'
+    const insert = req.body.email
+    console.log(req.body)
+    sql = mysql.format(sql,insert);
+    pool.query(sql,function(error,results){
+        if(error) throw error
+        const user = results
+        if(!user[0]) {return res.status(401).json({error : "Invalid Credential"}) }
+        
+        bcrypt.compare(req.body.pwd,user[0].pwd)
+        .then(result => {
+            if(!result){ return res.status(401).json({error : "invalid credential"})};
+            console.log(user[0].rôle)
+            res.status(200).json({rôle: user[0].rôle});     
+        })
+        .catch(error => { this.error = error })
+        })
+        
+               
+
+};
