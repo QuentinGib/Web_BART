7ae1adbf508dd821a00f3f78e0f0a16545e8080d
Quentin Gibon <quentingibon@wanadoo.fr>
2021-03-15 12:05:03 +0100

    howkey

--- client/src/views/HowKey.vue
+++ client/src/views/HowKey.vue
@@ -2,17 +2,52 @@
 <body>
   <navbar></navbar>
   <main>
-      <h1 id="theme">Comment utiliser sa clé privée ?</h1>
-      <h2>
-        Avec l'accès à cette plateforme vous a été fournie une clé privée.
-        Lorsqu'elle est demandée, veuillez renseigner cette clé dans l'espace correspondant,
-        puis cliquez sur "valider" pour effectuer l'action. Patientez quelques secondes puis un
-        message s'affichera vous informant de la réussite ou l'echec de votre action. Si tout se
-        passe bien, vous aurez réalisé votre première transaction sur la blockchain ! <br>
-        Attention : Ne divulguez jamais votre clé privée, et ne la perdez pas, elle est unique !
-      </h2>
+      <h1 id="theme">Comment utiliser sa clé publique et privée ?</h1>
+      <div class="Format_left">
+        <div id='Instruction'>
+          <h2>Instructions</h2>
+          <ul><p><strong>Personnes concernées:</strong></p>
+          <li>Les clients</li>
+          <li>Les entreprises</li>
+          <li>Les ressources</li>
+          </ul>
+          <p>Vous disposez d'une clé privée et d'une clé publique. Il ne faut pas perdre vos clés.
+            En cas de perte, merci de nous contacter à cette address mail <strong>email@gmail.com</strong></p>
+          <p>Vous n'aurez besoin que de votre clé privée pour signer les contrats.</p>
+          <p><strong>Ne divulgez jamais votre clé privée !</strong></p>
+        </div>
+        <div id="ValidateContrat">
+          <h2>Comment utiliser sa clé dans la validation de contrat</h2>
+          <ul><p><strong>Personnes concernées:</strong></p>
+          <li>Clients</li>
+          <li>Ressources</li>
+          </ul>
+          <p>-Allez dans la page Valider un contrat</p>
+          <p>-Choisissez l'id du contrat à valider</p>
+          <p>-Appuyez sur "signer le contrat</p>
+          <p>-Mettre sa clé privée et appuyez sur signer</p>
+          </div>
+        <div id='ValidateActivite'>
+          <h2>Comment utiliser sa clé dans la validation de l'activité</h2>
+         <ul><p><strong>Personnes concernées:</strong></p>
+          <li>Clients</li>
+         </ul>
+         <p>-Allez dans la page Valider l'activité</p>
+          <p>-Mettre sa clé privée</p>
+          <p>-Appuyez sur signer</p>
+        </div>
+      </div>
+        <aside class="Format_right">
+          <div>
+              <h1>Sommaire</h1>
+              <ul>
+                <li><a href="#Instruction">Instructions</a></li>
+                <li><a href="#ValidateContrat">La validation de contrat</a></li>
+                <li><a href="#ValidateActivite">La validation de l'activité</a></li>
+              </ul>
+          </div>
+        </aside>
   </main>
-  <foot></foot>
 </body>
 </template>
 <script>
@@ -25,5 +60,46 @@ export default {
 }
 </script>
 <style>
-
+.Format_left {
+  float: left;
+  width: 55%;
+}
+.Format_left div {
+  margin-top: 5px;
+  margin-bottom: 10rem;
+}
+.Format_left h2 {
+  font-size: 300%;
+  color: rgb(69, 111, 189);
+  text-decoration: underline black;
+}
+.Format_left ul{
+  font-size: 130%;
+  color: brown;
+}
+.Format_left ul>li {
+  font-size: 80%;
+  color: rgb(69, 111, 189);
+}
+.Format_left p {
+  margin: 0px;
+  font-size: 150%;
+}
+.Format_right {
+  width: 30%;
+  float: right;
+  margin-left: 5px;
+  margin-top: 5px;
+  position: sticky;
+  top:0;
+  background-color: gray;
+  color: white;
+}
+.Format_right ul>li>a {
+  color: black;
+  font-size: 150%;
+}
+aside h1 {
+  margin-bottom: 5px;
+}
 </style>
